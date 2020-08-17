cf062a166513f369e5058d42488320fbb6d8c836
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2020-08-17 22:08:40 +0600

    Splash Screen

---
+++ Application/app/src/main/java/com/example/dietary/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.dietary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //use a handler to creat a delay
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                finish();
+            }
+        },4000);
+    }
+
+}
