b642c9b2d4a58b6c6b9b80958895059619cfef13
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2020-09-13 15:05:56 +0600

    Firebase Authentication
    authentication using email & password

--- Application/app/src/main/java/com/example/dietary/UserProfileActivity.java
+++ Application/app/src/main/java/com/example/dietary/UserProfileActivity.java
@@ -2,13 +2,35 @@ package com.example.dietary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+
+        btnLogout = findViewById(R.id.logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(UserProfileActivity.this, LogInActivity.class);
+                startActivity(intToMain);
+            }
+        });
     }
 }
